cc182ad88fbd3f9324075238cef571bb21884aa2
atcg <evanmelstad@ucla.edu>
2014-05-31 23:32:25 -0700

    Fixed a file name

--- tortReadQC.pl
+++ tortReadQC.pl
@@ -14,12 +14,16 @@ my $adaptersDir;
 my $outDir;
 my $logFile;
 my $threadsMax = 4;
+my $trim;
+my $map;
 
 GetOptions  ("reads=s"         => \$readsDir,
              "adapters=s"      => \$adaptersDir,
              "out=s"           => \$outDir,
              "log=s"           => \$logFile,
              "threads=i"       => \$threadsMax,
+             "trim"            => \$trim,
+             "map"             => \$map,
              "help|man" => \$help) || pod2usage(2);
 
 if (!$readsDir or !$adaptersDir or !$outDir or $help) {
@@ -85,60 +89,80 @@ foreach my $file (@readsFiles) {
 print $logFH "--------------------------------------------------\n";
 print $logFH "Finished generating trimmomatic commands on all read files.\n\n\n";
 
-print $logFH "Running all trimmomatic commands\n";
-my $counter = 0;
-my $forkManager = new Parallel::ForkManager($threadsMax);
-foreach my $trimCommand (@trimmomaticCommands) {
-    $counter++;
+
+if ($trim) {
+    print $logFH "Running all trimmomatic commands\n";
+    my $counter = 0;
+    my $forkManager = new Parallel::ForkManager($threadsMax);
+    foreach my $trimCommand (@trimmomaticCommands) {
+        $counter++;
+        print $logFH "--------------------------------------------------\n";
+        print $logFH "Trimmomatic command $counter: \n\t"; # Indent the next line to make it easier to find the commands in the text
+        print $logFH $trimCommand . "\n";
+        sleep 10;
+        print "\n";
+        $forkManager->start and next;
+        print "\n";
+        system("$trimCommand");
+        print "Finished running the following:\n\t$trimCommand\n\n";
+        $forkManager->finish;
+    }
+    $forkManager->wait_all_children;
     print $logFH "--------------------------------------------------\n";
-    print $logFH "Trimmomatic command $counter: \n\t"; # Indent the next line to make it easier to find the commands in the text
-    print $logFH $trimCommand . "\n";
-    sleep 10;
-    print "\n";
-    $forkManager->start and next;
-    print "\n";
-    system("$trimCommand");
-    print "Finished running the following:\n\t$trimCommand\n\n";
-    $forkManager->finish;
-}
-$forkManager->wait_all_children;
-print $logFH "--------------------------------------------------\n";
-print $logFH "Finished running all trimmomatic commands\n";
-print $logFH "--------------------------------------------------\n\n";
-}
-
-my $fqjDir = $outDir . "/fastq-join";
-unless (-d $fqjDir) {
-    mkdir $fqjDir;
-}
-
-# print $logFH "Running fastq-join to merge overlapping paired end reads\n";
-foreach my $readGroup (sort keys %sampleNamesHash) {
-    sleep 10;
-    print $logFH "\n\nStarting to process $readGroup through the fastq-join and assembly stages\n\n";
-    my $combinedTrimmomaticSingles = $fqjDir . "/" . $readGroup . "_R1andR2trimmomaticSingles.fastq.gz";
-    my $R1pairedTrimmedUnzipped;
-    my $R2pairedTrimmedUnzipped;
-    if ($sampleNamesHash{$readGroup}{'R1_paired_trimmed'} =~ /(.*).gz$/) {
-        $R1pairedTrimmedUnzipped = $1;
-        system("gunzip $sampleNamesHash{$readGroup}{'R1_paired_trimmed'}");
+    print $logFH "Finished running all trimmomatic commands\n";
+    print $logFH "--------------------------------------------------\n\n";
     }
-    if ($sampleNamesHash{$readGroup}{'R2_paired_trimmed'} =~ /(.*).gz$/) {
-        $R2pairedTrimmedUnzipped = $1;
-        system("gunzip $sampleNamesHash{$readGroup}{'R2_paired_trimmed'}");
+    
+    my $fqjDir = $outDir . "/fastq-join";
+    unless (-d $fqjDir) {
+        mkdir $fqjDir;
     }
-    my $fqjOutputPrefix = $fqjDir . "/" . $readGroup . "_trimmed_fqj.%.fastq";
-    system("fastq-join -v ' ' $R1pairedTrimmedUnzipped $R2pairedTrimmedUnzipped -o $fqjOutputPrefix");
-    system("cat $sampleNamesHash{$readGroup}{'R1_singles_trimmed'} $sampleNamesHash{$readGroup}{'R2_singles_trimmed'} > $combinedTrimmomaticSingles");
-    system("gunzip $combinedTrimmomaticSingles");
-    my $joinedReads = $fqjDir . "/" . $readGroup . "_trimmed_fqj.join.fastq";
-    my $joinedAndSingles = $fqjDir . "/" . $readGroup . "_combinedJoinedAndSingles.fastq";
-    system("cat $joinedReads $combinedTrimmomaticSingles > $joinedAndSingles");
+    
+    # print $logFH "Running fastq-join to merge overlapping paired end reads\n";
+    foreach my $readGroup (sort keys %sampleNamesHash) {
+        sleep 10;
+        print $logFH "\n\nStarting to process $readGroup through the fastq-join and assembly stages\n\n";
+        my $combinedTrimmomaticSingles = $fqjDir . "/" . $readGroup . "_R1andR2trimmomaticSingles.fastq.gz";
+        my $R1pairedTrimmedUnzipped;
+        my $R2pairedTrimmedUnzipped;
+        if ($sampleNamesHash{$readGroup}{'R1_paired_trimmed'} =~ /(.*).gz$/) {
+            $R1pairedTrimmedUnzipped = $1;
+            system("gunzip $sampleNamesHash{$readGroup}{'R1_paired_trimmed'}");
+        }
+        if ($sampleNamesHash{$readGroup}{'R2_paired_trimmed'} =~ /(.*).gz$/) {
+            $R2pairedTrimmedUnzipped = $1;
+            system("gunzip $sampleNamesHash{$readGroup}{'R2_paired_trimmed'}");
+        }
+        my $fqjOutputPrefix = $fqjDir . "/" . $readGroup . "_trimmed_fqj.%.fastq";
+        system("fastq-join -v ' ' $R1pairedTrimmedUnzipped $R2pairedTrimmedUnzipped -o $fqjOutputPrefix");
+        system("cat $sampleNamesHash{$readGroup}{'R1_singles_trimmed'} $sampleNamesHash{$readGroup}{'R2_singles_trimmed'} > $combinedTrimmomaticSingles");
+        system("gunzip $combinedTrimmomaticSingles");
+        my $joinedReads = $fqjDir . "/" . $readGroup . "_trimmed_fqj.join.fastq";
+        my $joinedAndSingles = $fqjDir . "/" . $readGroup . "_combinedJoinedAndSingles.fastq";
+        # Need to update this file name since we unzipped the file above
+        if ($combinedTrimmomaticSingles =~ /(.*).gz$/) {
+            $combinedTrimmomaticSingles = $1;
+        }
+        
+        system("cat $joinedReads $combinedTrimmomaticSingles > $joinedAndSingles");
+    }
+    
+    print $logFH "--------------------------------------------------\n";
+    print $logFH "Finished running fastq-join on all samples\n";
+    print $logFH "--------------------------------------------------\n\n";
 }
 
-print $logFH "--------------------------------------------------\n";
-print $logFH "Finished running fastq-join on all samples\n";
-print $logFH "--------------------------------------------------\n\n"; 
+if ($map) {
+    foreach my $readGroup (sort keys %sampleNamesHash) {
+        
+        
+        
+        
+    }
+}
+
+
+
 
 
 
@@ -162,6 +186,10 @@ perl tortReadQC.pl --reads <file> --adapters <file>
  Options:
    -reads=s         Directory with raw reads in gzipped fastq format
    -adapters=s      Directory with adapters fasta files
+   -out             Name of output directory (it will be created)
+   -log             Name of logfile to print output (you will probably also want
+                    to capture STDERR manually)
+   -threads         Threads to use for multithreading (default=4)
    -help|man        Prints out documentation
 
 
